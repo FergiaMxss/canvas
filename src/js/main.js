bcae86cad050d9de19d3174534d0d75a69eb1f7f
Vladislav <blabla>
2023-03-12 14:27:47 +0300

    create mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextinputs from './modules/checkTextinputs';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -9,4 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
-})
+    mask('[name="phone"]');
+    checkTextinputs('[name="name"]');
+    checkTextinputs('[name="message"]');
+});
